ba78d8df979dd312f9da5caf1ca2805d69ccbf05
James Sun <chuwen.sun@yahoo.com>
2021-11-28 20:23:48 -0500

    Added no internet access solution

--- app/src/main/java/com/example/checkpoint3for5236/MainActivity.java
+++ app/src/main/java/com/example/checkpoint3for5236/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.checkpoint3for5236;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
@@ -107,10 +110,31 @@ public class MainActivity extends AppCompatActivity {
                     //redirect to user profile
                     startActivity(new Intent(MainActivity.this, ChooseDiscussionBoardActivity.class));
                 }else {
-                    Toast.makeText(MainActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                    if(!haveNetworkConnection()){
+                        Toast.makeText(MainActivity.this, "Failed to login! Please check your internet access", Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(MainActivity.this, "Failed to login! Please check your credentials", Toast.LENGTH_LONG).show();
+                    }
                 }
             }
         });
 
+    }
+
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
     }
 }
